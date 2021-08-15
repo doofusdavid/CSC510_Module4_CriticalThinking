1c63f3177e0941ba18bac3c09c0b405edd161f1d
David Edwards <david.edwards@gmail.com>
2021-08-15 09:06:24 -0600

    walking with mountains doesn't return, refactoring

---
+++ walking.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from __future__ import print_function
+
+import math
+from simpleai.search import SearchProblem, astar
+
+MAP = """
+##############################
+#         M              #   #
+# MMMM    MMMMMMMM       #   #
+#  o #    M              #   #
+#    #MM     MMMM   MMMMMM   #
+#         MMMM      M        #
+#            M  M   M   MMMM #
+#     MMMMMM    M       M x  #
+#        M      M            #
+##############################
+"""
+MAP = [list(x) for x in MAP.split("\n") if x]
+
+COSTS = {
+    "up": 1.0,
+    "down": 1.0,
+    "left": 1.0,
+    "right": 1.0,
+    "up left": 1.4,
+    "up right": 1.4,
+    "down left": 1.4,
+    "down right": 1.4,
+    "up mountain": 2.0,
+    "down mountain": 2.0,
+    "left mountain": 2.0,
+    "right mountain": 2.0,
+    "up left mountain": 10.0,
+    "up right mountain": 10.0,
+    "down left mountain": 10.0,
+    "down right mountain": 10.0
+}
+
+
+class GameWalkPuzzle(SearchProblem):
+
+    def __init__(self, board):
+        self.board = board
+        self.goal = (0, 0)
+        for y in range(len(self.board)):
+            for x in range(len(self.board[y])):
+                if self.board[y][x].lower() == "o":
+                    self.initial = (x, y)
+                elif self.board[y][x].lower() == "x":
+                    self.goal = (x, y)
+
+        super(GameWalkPuzzle, self).__init__(initial_state=self.initial)
+
+    def actions(self, state):
+        actions = []
+        for action in list(COSTS.keys()):
+            newx, newy = self.result(state, action)
+            if self.board[newy][newx] != "#":
+                if self.board[newy][newx] == "M" and action.count("mountain"):
+                    actions.append(action)
+                elif self.board[newy][newx] == " " and action.count("mountain") == 0:
+                    actions.append(action)
+        return actions
+
+    def result(self, state, action):
+        x, y = state
+
+        if action.count("up"):
+            y -= 1
+        if action.count("down"):
+            y += 1
+        if action.count("left"):
+            x -= 1
+        if action.count("right"):
+            x += 1
+
+        new_state = (x, y)
+        return new_state
+
+    def is_goal(self, state):
+        return state == self.goal
+
+    def cost(self, state, action, state2):
+        return COSTS[action]
+
+    def heuristic(self, state):
+        x, y = state
+        gx, gy = self.goal
+        return abs(x-gx) + abs(y-gy)
+        # return math.sqrt((x - gx) ** 2 + (y - gy) ** 2)
+
+
+def main():
+    problem = GameWalkPuzzle(MAP)
+    result = astar(problem, graph_search=True)
+    path = [x[1] for x in result.path()]
+
+    for y in range(len(MAP)):
+        for x in range(len(MAP[y])):
+            if (x, y) == problem.initial:
+                print("o", end='')
+            elif (x, y) == problem.goal:
+                print("x", end='')
+            elif (x, y) in path:
+                print("·", end='')
+            else:
+                print(MAP[y][x], end='')
+        print()
+
+
+if __name__ == "__main__":
+    main()
